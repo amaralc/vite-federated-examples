ed10f6713f2eab26269ef44f14260d98566226c4
amaralc <amaral.calil@gmail.com>
2022-10-23 20:30:17 -0300

    feat: centralize section content

--- apps/remote-vite-plugin/src/components/Section.jsx
+++ apps/remote-vite-plugin/src/components/Section.jsx
@@ -1,6 +1,6 @@
 const Section = () => {
     return (
-        <section style={{borderRadius: 10, border: 'solid 1px red', padding: '1 rem', width: '100%', height: '100px'}}>
+        <section style={{borderRadius: 10, border: 'solid 1px red', padding: '1 rem', width: '100%', height: '100px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
             Section
         </section>
     )
